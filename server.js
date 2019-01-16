bf8cd2442d2402716b83cbcf5e7391b34991a0f7
“IBayon“ <IBayon@Enova.com>
2019-01-15 22:29:40 -0600

    setting up initial files

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 // Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
-var logger = require("morgan");
-var path = require("path");
-
+var exphbs  = require('express-handlebars');
+var mongoose = require('mongoose');
+var cheerio = require('cheerio');
+var axios = require("axios");
+//var mongojs = require("mongojs");
 var app = express();
+
+// If deployed to Heroku, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+
+
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
